77572754e121c25850511fbb771b51b7af08adc8
Steve-M98 <shoyadbekmaxmudov@gmail.com>
2026-09-20 20:07:36 +0900

    feat: MIT TTASK ' L '  bajarildi

---
+++ src/train.js
@@ -0,0 +1,15 @@
+/* 
+TASK L
+
+So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
+
+Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc" */
+
+function taskL(str) {
+  return str
+    .split(" ") // so'zlarga ajratadi ("") va (" ") farqi 2 si so'zga ajratdi probelga qarab
+    .map((word) => word.split("").reverse().join(""))
+    .join(" "); // ("") va (" " ) bu ham shunday probelga qarab qayta birlashtiradi
+}
+
+console.log(taskL("Men bugundan Burak loyihasini boshladim"));
