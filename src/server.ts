1af2abf69d152ab8a2ab3e13ee780a01eb33dc80
Steve-M98 <shoyadbekmaxmudov@gmail.com>
2026-09-23 13:48:56 +0900

    feat: Integration of Environmental Variables

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,10 @@
 // MVC = MODEL VIEW CONTROLLER
 
 // Design pattern: Middleware, Decotar
+
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL", process.env.MONGO_URL);
